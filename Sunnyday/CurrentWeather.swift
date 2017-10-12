9f9313ff5aad00b82b5a8fc6d2651625852ae393
parth <parth.v.bhoiwala@gmail.com>
2017-10-12 00:08:52 -0400

    refactored weather class

---
+++ Sunnyday/CurrentWeather.swift
@@ -0,0 +1,22 @@
+//
+//  CurrentWeather.swift
+//  Sunnyday
+//
+//  Created by Parth on 10/11/17.
+//  Copyright © 2017 Bhoiwala. All rights reserved.
+//
+
+class CurrentWeather {
+    
+    var temperature: Int = 0
+    var high_temp: Int = 0
+    var low_temp: Int = 0
+    var humidity: Int = 0
+    var wind_mph: Int = 0
+    var wind_dir: String = ""
+    var location: String = ""
+    var condition: String = ""
+    
+    init () {}
+    
+}
--- Sunnyday/ViewController.swift
+++ Sunnyday/ViewController.swift
@@ -10,13 +10,13 @@ import UIKit
 
 class ViewController: UIViewController, UIPageViewControllerDelegate {
     
-    @IBOutlet weak var currentTemp: UILabel!
-    @IBOutlet weak var currentForecast: UILabel!
-    @IBOutlet weak var city: UILabel!
-    @IBOutlet weak var high: UILabel!
-    @IBOutlet weak var low: UILabel!
-    @IBOutlet weak var humidity: UILabel!
-    @IBOutlet weak var wind: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var conditionLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var highTempLabel: UILabel!
+    @IBOutlet weak var lowTempLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var windLabel: UILabel!
     @IBOutlet weak var imgView: UIImageView!
     
     let forecastUrl:String = "https://api.apixu.com/v1/forecast.json?key=e763d5cf81a040e89b925722171605&q=Seattle"
@@ -35,8 +35,8 @@ class ViewController: UIViewController, UIPageViewControllerDelegate {
         super.viewDidLoad()
         let forecastJson:NSDictionary = getWeatherJson(urlType: forecastUrl)
         print(forecastJson)
-        parseWeatherInfo(weatherJson: forecastJson)
-        refreshUI()
+        let currentWeather = parseWeatherInfo(weatherJson: forecastJson)
+        refreshUI(weather: currentWeather)
         createGradientLayer()
         
         
@@ -46,46 +46,74 @@ class ViewController: UIViewController, UIPageViewControllerDelegate {
     
     
     
-    func parseWeatherInfo(weatherJson: NSDictionary){
+    func parseWeatherInfo(weatherJson: NSDictionary) -> CurrentWeather {
+        let currentWeather = CurrentWeather()
         if let locationDict = weatherJson["location"] as? NSDictionary{
-            location = locationDict["name"] as! String
+//            location = locationDict["name"] as! String
+            currentWeather.location = locationDict["name"] as! String
         }
+        
         if let currentDict = weatherJson["current"] as? NSDictionary{
-            tempf = Int(currentDict["temp_f"] as! NSNumber)
-            windMph = (Int(currentDict["wind_mph"] as! NSNumber))
-            windDir = (currentDict["wind_dir"] as? String)!
-            humid = (Int(currentDict["humidity"] as! NSNumber))
+//            tempf = Int(currentDict["temp_f"] as! NSNumber)
+//            windMph = (Int(currentDict["wind_mph"] as! NSNumber))
+//            windDir = (currentDict["wind_dir"] as? String)!
+//            humid = (Int(currentDict["humidity"] as! NSNumber))
+            
+            currentWeather.temperature = Int(currentDict["temp_f"] as! NSNumber)
+            currentWeather.humidity = (Int(currentDict["humidity"] as! NSNumber))
+            currentWeather.wind_mph = (Int(currentDict["wind_mph"] as! NSNumber))
+            currentWeather.wind_dir = (currentDict["wind_dir"] as? String)!
+            
+            
+            
+            
             if let cond = currentDict["condition"] as? NSDictionary{
-                condition = cond["text"] as! String
+//                condition = cond["text"] as! String
+                currentWeather.condition = cond["text"] as! String
             }
-            imgView.image = setIconForCondition(condition: condition).withRenderingMode(.alwaysTemplate)
-            imgView.tintColor = UIColor.white
+            
+//            imgView.image = setIconForCondition(condition: condition).withRenderingMode(.alwaysTemplate)
+//            imgView.tintColor = UIColor.white
         }
+        
         if let forecastDict = weatherJson["forecast"] as? NSDictionary{
             if let forecastDay = forecastDict["forecastday"] as? NSArray{
                 let fDay = forecastDay[0] as? NSDictionary
                 if let day = fDay?["day"] as? NSDictionary{
-                    dayHigh = Int(day["maxtemp_f"] as! NSNumber)
-                    dayLow = Int(day["mintemp_f"] as! NSNumber)
+//                    dayHigh = Int(day["maxtemp_f"] as! NSNumber)
+//                    dayLow = Int(day["mintemp_f"] as! NSNumber)
+                    currentWeather.low_temp = Int(day["mintemp_f"] as! NSNumber)
+                    currentWeather.high_temp = Int(day["maxtemp_f"] as! NSNumber)
                 }
             }
         }
         
+      return currentWeather
+    }
+    
+    
+    
+    func refreshUI(weather: CurrentWeather ){
+//        currentTempLabel.text = String(tempf)
+//        windLabel.text = String(windMph) + " " + windDir
+//        locationLabel.text = location
+//        conditionLabel.text = condition
+//        humidityLabel.text = String(humid) + "%"
+//        lowTempLabel.text = String(dayLow)
+//        highTempLabel.text = String(dayHigh)
+        
+        currentTempLabel.text = String(weather.temperature)
+        windLabel.text = String(weather.wind_mph) + " " + weather.wind_dir
+        locationLabel.text = weather.location
+        conditionLabel.text = weather.condition
+        humidityLabel.text = String(weather.humidity) + "%"
+        lowTempLabel.text = String(weather.low_temp)
+        highTempLabel.text = String(weather.high_temp)
+        
+        imgView.image = setIconForCondition(condition: condition).withRenderingMode(.alwaysTemplate)
+        imgView.tintColor = UIColor.white
         
     }
-    
-    
-    
-    func refreshUI(){
-        currentTemp.text = String(tempf)
-        wind.text = String(windMph) + " " + windDir
-        city.text = location
-        currentForecast.text = condition
-        humidity.text = String(humid) + "%"
-        low.text = String(dayLow)
-        high.text = String(dayHigh)
-    }
-    
     
     func getWeatherJson(urlType: String) -> NSDictionary{
         let semaphore = DispatchSemaphore(value: 0)
